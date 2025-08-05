80bec3f88817f0f2640b1db67cfb48a355b9f766
MUKULIKA PANDA <mukulipa@in.ibm.com>
2025-08-05 12:05:48 +0530

    Add code coverage step

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
-from main import add
+from main import add # importing the add function from a file named main.py
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 5 # if it is giving 5 or not
     assert add(0, 0) == 0
